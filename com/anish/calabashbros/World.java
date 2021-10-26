8215fbf3f24142352397ed641073f049d659d0f7
Lian jianheng <583761743@qq.com>
2021-10-26 21:07:44 +0800

    I don't know

--- com/anish/calabashbros/World.java
+++ com/anish/calabashbros/World.java
@@ -2,8 +2,8 @@ package com.anish.calabashbros;
 
 public class World {
 
-    public static final int WIDTH = 40;
-    public static final int HEIGHT = 20;
+    public static final int WIDTH = 50;
+    public static final int HEIGHT = 50;
 
     private Tile<Thing>[][] tiles;
 

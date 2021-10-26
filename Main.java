4c006550eb2ef54821f96b9c96ff7608bf02f0b9
Lian jianheng <583761743@qq.com>
2021-10-26 21:20:30 +0800

    some edit

--- Main.java
+++ Main.java
@@ -1,57 +1,63 @@
 
 
+import asciiPanel.AsciiFont;
+import asciiPanel.AsciiPanel;
+import com.anish.calabashbros.World;
+import com.anish.screen.Screen;
+import mazegenerator.MazeScreen;
+
+import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-import javax.swing.JFrame;
-
-import com.anish.calabashbros.World;
-import com.anish.screen.Screen;
-import com.anish.screen.WorldScreen;
-
-import asciiPanel.AsciiFont;
-import asciiPanel.AsciiPanel;
-
-public class Main extends JFrame implements KeyListener {
+public class Main extends JFrame implements KeyListener
+{
 
     private AsciiPanel terminal;
     private Screen screen;
 
-    public Main() {
+    public Main()
+    {
         super();
         terminal = new AsciiPanel(World.WIDTH, World.HEIGHT, AsciiFont.TALRYTH_15_15);
         add(terminal);
         pack();
-        screen = new WorldScreen();
+        screen = new MazeScreen();
         addKeyListener(this);
         repaint();
 
     }
 
     @Override
-    public void repaint() {
+    public void repaint()
+    {
         terminal.clear();
         screen.displayOutput(terminal);
         super.repaint();
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e)
+    {
 
     }
 
     @Override
-    public void keyPressed(KeyEvent e) {
+    public void keyPressed(KeyEvent e)
+    {
         screen = screen.respondToUserInput(e);
         repaint();
+
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
+    public void keyReleased(KeyEvent e)
+    {
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Main app = new Main();
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
